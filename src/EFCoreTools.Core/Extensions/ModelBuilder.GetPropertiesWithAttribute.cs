3e459658db61262477ff23f18c3204217aa9658c
Alex Bello <alex.bello@uberops.com>
2017-06-23 19:29:05 -0400

    Updated IndexAttribute Convetions - Does not build

--- src/EFCoreTools.Core/Extensions/ModelBuilder.GetPropertiesWithAttribute.cs
+++ src/EFCoreTools.Core/Extensions/ModelBuilder.GetPropertiesWithAttribute.cs
@@ -8,15 +8,13 @@ namespace EFCoreTools.Core.Extensions
 {
     public static class AttributeExtensionMethods
     {
-        public static List<TypePropertiesWithAttributeList> GetPropertiesWithAttribute<T>(this ModelBuilder modelBuilder) 
+        public static IEnumerable<Type> GetAllPropertiesWithAttribute<T>(this ModelBuilder modelBuilder) 
             where T : Attribute
         {
-            var d = modelBuilder.Model.GetEntityTypes()
-            .SelectMany(t => t.ClrType.GetProperties().Where(p => p.IsDefined(typeof(T), false)), 
-                (t, p) => new TypePropertiesWithAttributeList(t, p))
-            .ToList();
-
-            return d;
+            return modelBuilder.Model.GetEntityTypes()
+            .Select(i => i.ClrType)
+            .Where(t => t.GetProperties().Where(p => p.IsDefined(typeof(T), false)).Any());
+            //.SelectMany(t => t.GetProperties().Where(p => p.IsDefined(typeof(T), false)));
         }
     }
 }
