e61bf538c879a6da1da0d3e2950dbcb4bec0e36c
Alex Bello <alex.bello@flhealth.gov>
2017-06-26 14:30:37 -0400

    Added Tests for IndexConvention

--- tests/EFCoreTools.Tests/ConventionTests.cs
+++ tests/EFCoreTools.Tests/ConventionTests.cs
@@ -2,6 +2,8 @@ using EFCoreTools.Tests.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
 
 namespace EFCoreTools.Tests
 {
@@ -9,15 +11,31 @@ namespace EFCoreTools.Tests
     public class ConventionTests
     {
         [TestMethod]
-        public void GetAllTypePropertyAttributesTest()
+        public void IndexConventionTests()
         {
             // Assign
-            var model = new ModelBuilder(new CoreConventionSetBuilder().CreateConventionSet());
-            
+            var options = new DbContextOptionsBuilder<TestDbContext>()
+                .UseInMemoryDatabase(databaseName: "index_convention_tests")
+                .Options;
+
             // Act
-            //model.Entity<Person>().
+            var context = new TestDbContext(options);
+            var personEntity = context.Model.FindEntityType(typeof(Person));
+            var indexes = personEntity.GetIndexes();
+
+            foreach (var index in indexes)
+            {
+                Console.WriteLine(string.Format("DeclaringEntityType: {0}", index.DeclaringEntityType.ClrType.Name));
+                Console.WriteLine(string.Format("IsUnique: {0}", index.IsUnique));
+                Console.WriteLine(string.Format("IndexName: {0}", index.Relational().Name));
+                Console.WriteLine("");
+            }
 
             // Assert
+            Assert.IsNotNull(personEntity);
+            Assert.AreEqual(indexes.Count(), 2);
+            Assert.IsTrue(indexes.Any(x => x.Relational().Name == "IX_Person_LastName"));
+            Assert.IsTrue(indexes.Any(x => x.Relational().Name == "IX_Person_SSN_ZipCode"));
         }
     }
 }
--- tests/EFCoreTools.Tests/Models/TestDbContext.cs
+++ tests/EFCoreTools.Tests/Models/TestDbContext.cs
@@ -1,13 +1,24 @@
+using EFCoreTools.Core.Conventions;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreTools.Tests.Models
 {
     public class TestDbContext : DbContext
     {
-        protected override void OnModelCreating(ModelBuilder builder)
+        public TestDbContext() { }
+
+        public TestDbContext(DbContextOptions<TestDbContext> options)
+            : base(options)
         {
 
-            
+        }
+
+        public DbSet<Person> Person { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            new IndexConvention().Apply(builder);
+            base.OnModelCreating(builder);
         }
         
     }
